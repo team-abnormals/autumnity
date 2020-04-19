0efad2d027925a1cb4580a04776626a447b4448e
Markus1002 <51504784+Markus1002@users.noreply.github.com>
2020-04-19 17:03:29 +0300

    Stinky pies

--- src/main/java/com/markus1002/autumnity/common/item/ModFoods.java
+++ src/main/java/com/markus1002/autumnity/common/item/ModFoods.java
@@ -7,5 +7,6 @@ import net.minecraft.potion.Effects;
 public class ModFoods
 {
 	public static final Food SYRUP_BOTTLE = (new Food.Builder()).hunger(4).saturation(0.3F).build();
-	public static final Food FOUL_BERRIES = (new Food.Builder()).hunger(2).saturation(0.1F).effect(new EffectInstance(Effects.HUNGER, 400, 0), 0.8F).build();
+	public static final Food FOUL_BERRIES = (new Food.Builder()).hunger(2).saturation(0.1F).effect(new EffectInstance(Effects.HUNGER, 300, 0), 0.8F).build();
+	public static final Food FOUL_BERRY_PIE = (new Food.Builder()).hunger(8).saturation(0.3F).effect(new EffectInstance(Effects.HUNGER, 300, 0), 0.3F).build();
 }
--- src/main/java/com/markus1002/autumnity/core/registry/ModItems.java
+++ src/main/java/com/markus1002/autumnity/core/registry/ModItems.java
@@ -79,6 +79,7 @@ public class ModItems
 	public static RegistryObject<Item> FOUL_BERRIES = ITEMS.register("foul_berries", ModList.get().isLoaded("berry_good") ? () -> new Item((new Item.Properties()).group(ItemGroup.FOOD).food(ModFoods.FOUL_BERRIES)) : () -> new BlockNamedItem(ModBlocks.FOUL_BERRY_BUSH.get(), (new Item.Properties()).group(ItemGroup.FOOD).food(ModFoods.FOUL_BERRIES)));
 	public static RegistryObject<Item> FOUL_BERRY_PIPS = ITEMS.register("foul_berry_pips", () -> new BlockNamedItem(ModBlocks.FOUL_BERRY_BUSH_PIPS.get(), (new Item.Properties()).group(ModList.get().isLoaded("berry_good") ? ItemGroup.MISC : null)));
 	public static RegistryObject<Item> FOUL_BERRY_LEAF = ITEMS.register("foul_berry_leaf", () -> new Item((new Item.Properties()).group(ItemGroup.BREWING)));
+	public static RegistryObject<Item> FOUL_BERRY_PIE = ITEMS.register("foul_berry_pie", () -> new Item((new Item.Properties()).group(ItemGroup.FOOD).food(ModFoods.FOUL_BERRY_PIE)));
 	public static RegistryObject<Item> SNAIL_SPAWN_EGG = ITEMS.register("snail_spawn_egg", () -> new SpawnEggItem(ModEntities.SNAIL, 7355937, 14727558, (new Item.Properties()).group(ItemGroup.MISC)));
 	public static RegistryObject<Item> SNAIL_SHELL_PIECE = ITEMS.register("snail_shell_piece", () -> new Item((new Item.Properties()).group(ItemGroup.MATERIALS)));
 	public static RegistryObject<Item> SNAIL_SHELL_CHESTPLATE = ITEMS.register("snail_shell_chestplate", () -> new SnailShellChestplateItem(SNAIL_SHELL_MATERIAL, EquipmentSlotType.CHEST, (new Item.Properties()).group(ItemGroup.COMBAT)));

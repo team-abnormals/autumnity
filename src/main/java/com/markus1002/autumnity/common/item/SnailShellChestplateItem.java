1ede718a5d06dd2fa10d5630e0de209f30b9334e
Markus1002 <51504784+Markus1002@users.noreply.github.com>
2020-07-21 21:51:21 +0300

    Added tooltips for the snail shell chestplate

--- src/main/java/com/markus1002/autumnity/common/item/SnailShellChestplateItem.java
+++ src/main/java/com/markus1002/autumnity/common/item/SnailShellChestplateItem.java
@@ -1,10 +1,15 @@
 package com.markus1002.autumnity.common.item;
 
+import java.util.List;
+
 import javax.annotation.Nullable;
 
 import com.markus1002.autumnity.core.util.Reference;
 
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
@@ -12,7 +17,13 @@ import net.minecraft.item.IArmorMaterial;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class SnailShellChestplateItem extends ArmorItem
 {
@@ -36,4 +47,13 @@ public class SnailShellChestplateItem extends ArmorItem
 	{
 		return Reference.location("textures/models/armor/snail_shell_layer_1.png").toString();
 	}
+	
+    @OnlyIn(Dist.CLIENT)
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn)
+    {
+    	tooltip.add(StringTextComponent.EMPTY);
+    	tooltip.add((new TranslationTextComponent("item.autumnity.snail_shell_chestplate.whenSneaking").func_240699_a_(TextFormatting.GRAY)));
+    	tooltip.add(new TranslationTextComponent(Effects.RESISTANCE.getName()).func_240699_a_(TextFormatting.BLUE).func_240702_b_(" ").func_230529_a_(new TranslationTextComponent("potion.potency.2").func_240699_a_(TextFormatting.BLUE)));
+    	tooltip.add((new TranslationTextComponent("attribute.modifier.plus." + AttributeModifier.Operation.ADDITION.getId(), ItemStack.DECIMALFORMAT.format(10), new TranslationTextComponent(Attributes.KNOCKBACK_RESISTANCE.func_233754_c_()))).func_240699_a_(TextFormatting.BLUE));
+    }
 }

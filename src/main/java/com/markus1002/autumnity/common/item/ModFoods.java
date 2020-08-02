707478183b2da3df2435beae1a0ee1ced968de43
Markus1002 <51504784+Markus1002@users.noreply.github.com>
2020-08-02 11:56:39 +0300

    Tweaked some textures

--- src/main/java/com/markus1002/autumnity/common/item/ModFoods.java
+++ src/main/java/com/markus1002/autumnity/common/item/ModFoods.java
@@ -9,6 +9,6 @@ import net.minecraft.potion.Effects;
 public class ModFoods
 {
 	public static final Food SYRUP_BOTTLE = (new Food.Builder()).hunger(4).saturation(0.3F).build();
-	public static final Food FOUL_BERRIES = (new Food.Builder()).hunger(2).saturation(0.1F).fastToEat().effect(new EffectInstance(ModEffects.FOUL_TASTE, 1200, 0), 1.0F).effect(new EffectInstance(Effects.HUNGER, 600, 0), 0.8F).build();
+	public static final Food FOUL_BERRIES = (new Food.Builder()).hunger(2).saturation(0.1F).effect(new EffectInstance(ModEffects.FOUL_TASTE, 1200, 0), 1.0F).effect(new EffectInstance(Effects.HUNGER, 600, 0), 0.8F).build();
 	public static final Food FOUL_BERRY_PIE = (new Food.Builder()).hunger(6).saturation(0.3F).effect(new EffectInstance(ModEffects.FOUL_TASTE, 1800, 1), 1.0F).build();
 }
--- src/main/java/com/markus1002/autumnity/common/item/SnailShellChestplateItem.java
+++ src/main/java/com/markus1002/autumnity/common/item/SnailShellChestplateItem.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.annotation.Nullable;
 
 import com.markus1002.autumnity.core.util.Reference;
+import com.teamabnormals.abnormals_core.core.utils.ItemStackUtils;
 
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
@@ -14,9 +15,12 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
@@ -56,4 +60,21 @@ public class SnailShellChestplateItem extends ArmorItem
     	tooltip.add(new TranslationTextComponent(Effects.RESISTANCE.getName()).func_240699_a_(TextFormatting.BLUE).func_240702_b_(" ").func_230529_a_(new TranslationTextComponent("potion.potency.2").func_240699_a_(TextFormatting.BLUE)));
     	tooltip.add((new TranslationTextComponent("attribute.modifier.plus." + AttributeModifier.Operation.ADDITION.getId(), ItemStack.DECIMALFORMAT.format(10), new TranslationTextComponent(Attributes.KNOCKBACK_RESISTANCE.func_233754_c_()))).func_240699_a_(TextFormatting.BLUE));
     }
+    
+	@Override
+	public void fillItemGroup(ItemGroup group, NonNullList<ItemStack> items)
+	{
+		if(ItemStackUtils.isInGroup(this.asItem(), group))
+		{
+			int targetIndex = ItemStackUtils.findIndexOfItem(Items.TURTLE_HELMET, items);
+			if(targetIndex != -1)
+			{
+				items.add(targetIndex + 1, new ItemStack(this));
+			}
+			else
+			{
+				super.fillItemGroup(group, items);
+			}
+		}
+	}
 }

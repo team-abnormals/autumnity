5c07086d8e7b3150c17db1fe910ceab15694ce16
Markus1002 <51504784+Markus1002@users.noreply.github.com>
2020-08-06 10:48:53 +0300

    Foul berries no longer inflict hunger
    This is to not discourage people from eating it. They also now restore only 1 hunger point when eaten, making them bad sources of food on their own.

--- src/main/java/com/markus1002/autumnity/common/item/ModFoods.java
+++ src/main/java/com/markus1002/autumnity/common/item/ModFoods.java
@@ -4,11 +4,10 @@ import com.markus1002.autumnity.core.registry.ModEffects;
 
 import net.minecraft.item.Food;
 import net.minecraft.potion.EffectInstance;
-import net.minecraft.potion.Effects;
 
 public class ModFoods
 {
 	public static final Food SYRUP_BOTTLE = (new Food.Builder()).hunger(4).saturation(0.3F).build();
-	public static final Food FOUL_BERRIES = (new Food.Builder()).hunger(2).saturation(0.1F).effect(new EffectInstance(ModEffects.FOUL_TASTE, 1200, 0), 1.0F).effect(new EffectInstance(Effects.HUNGER, 600, 0), 0.8F).build();
-	public static final Food FOUL_BERRY_PIE = (new Food.Builder()).hunger(6).saturation(0.3F).effect(new EffectInstance(ModEffects.FOUL_TASTE, 1800, 1), 1.0F).build();
+	public static final Food FOUL_BERRIES = (new Food.Builder()).hunger(1).saturation(0.1F).effect(new EffectInstance(ModEffects.FOUL_TASTE, 1200, 0), 1.0F).build();
+	public static final Food FOUL_BERRY_PIE = (new Food.Builder()).hunger(4).saturation(0.3F).effect(new EffectInstance(ModEffects.FOUL_TASTE, 1800, 1), 1.0F).build();
 }

bbca56baeb32d344de0f7661bab2c86bee67ea01
Markus1002 <51504784+Markus1002@users.noreply.github.com>
2020-08-18 15:14:53 +0300

    Fixes #10

--- src/main/java/com/markus1002/autumnity/core/other/VanillaCompatibility.java
+++ src/main/java/com/markus1002/autumnity/core/other/VanillaCompatibility.java
@@ -74,6 +74,7 @@ public class VanillaCompatibility
 		DataUtils.registerFlammable(AutumnityBlocks.MAPLE_VERTICAL_SLAB.get(), 5, 20);
 		DataUtils.registerFlammable(AutumnityBlocks.VERTICAL_MAPLE_PLANKS.get(), 5, 20);
 		DataUtils.registerFlammable(AutumnityBlocks.MAPLE_BOOKSHELF.get(), 30, 20);
+		DataUtils.registerFlammable(AutumnityBlocks.FOUL_BERRY_SACK.get(), 30, 60);
 	}
 
 	public static void setupVanillaCompatibilityClient()

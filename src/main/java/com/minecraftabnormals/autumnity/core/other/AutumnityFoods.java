128e7bf3a194f49a2d6e88c7db29d4b38b12ac33
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2021-01-22 16:26:20 -0600

    Turkey Pieces are now fast to eat

--- src/main/java/com/minecraftabnormals/autumnity/core/other/AutumnityFoods.java
+++ src/main/java/com/minecraftabnormals/autumnity/core/other/AutumnityFoods.java
@@ -9,6 +9,6 @@ public class AutumnityFoods {
 	public static final Food SYRUP_BOTTLE = (new Food.Builder()).hunger(4).saturation(0.3F).build();
 	public static final Food FOUL_BERRIES = (new Food.Builder()).hunger(1).saturation(0.1F).effect(() -> new EffectInstance(AutumnityEffects.FOUL_TASTE.get(), 320, 0), 1.0F).build();
 	public static final Food PUMPKIN_BREAD = (new Food.Builder()).hunger(8).saturation(0.4F).build();
-	public static final Food TURKEY = (new Food.Builder()).hunger(1).saturation(0.3F).meat().effect(() -> new EffectInstance(Effects.HUNGER, 600, 0), 0.1F).build();
-	public static final Food COOKED_TURKEY = (new Food.Builder()).hunger(2).saturation(0.6F).meat().build();
+	public static final Food TURKEY = (new Food.Builder()).hunger(1).saturation(0.3F).meat().effect(() -> new EffectInstance(Effects.HUNGER, 600, 0), 0.1F).fastToEat().build();
+	public static final Food COOKED_TURKEY = (new Food.Builder()).hunger(2).saturation(0.6F).meat().fastToEat().build();
 }

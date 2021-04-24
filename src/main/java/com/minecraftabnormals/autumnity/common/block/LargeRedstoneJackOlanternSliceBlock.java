b8f77f9aba2ff34f35ccbef20fe1c3f3afbdbdb1
Markus1002 <51504784+Markus1002@users.noreply.github.com>
2021-04-24 20:01:29 +0300

    Reverted redstone pumpkins being a redstone power source

--- src/main/java/com/minecraftabnormals/autumnity/common/block/LargeRedstoneJackOlanternSliceBlock.java
+++ src/main/java/com/minecraftabnormals/autumnity/common/block/LargeRedstoneJackOlanternSliceBlock.java
@@ -1,16 +1,12 @@
 package com.minecraftabnormals.autumnity.common.block;
 
-import com.minecraftabnormals.autumnity.common.block.properties.CarvedSide;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.RedstoneTorchBlock;
 import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.StateContainer;
-import net.minecraft.util.Direction;
-import net.minecraft.util.Direction.Axis;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 
@@ -22,81 +18,30 @@ public class LargeRedstoneJackOlanternSliceBlock extends CarvedLargePumpkinSlice
 
 	public LargeRedstoneJackOlanternSliceBlock(Properties properties) {
 		super(properties);
-		this.setDefaultState(this.getDefaultState().with(LIT, Boolean.TRUE));
+		this.setDefaultState(this.getDefaultState().with(LIT, false));
 	}
 
 	@Nullable
 	@Override
 	public BlockState getStateForPlacement(BlockItemUseContext context) {
-		BlockState blockstate = super.getStateForPlacement(context);
-		return blockstate.with(LIT, !isBlockPowered(blockstate, context.getWorld(), context.getPos()));
-	}
-
-	@Override
-	public void onBlockAdded(BlockState state, World worldIn, BlockPos pos, BlockState oldState, boolean isMoving) {
-		for (Direction direction : Direction.values()) {
-			worldIn.notifyNeighborsOfStateChange(pos.offset(direction), this);
-		}
-	}
-
-	@Override
-	public void onReplaced(BlockState state, World worldIn, BlockPos pos, BlockState newState, boolean isMoving) {
-		if (!isMoving) {
-			for (Direction direction : Direction.values()) {
-				worldIn.notifyNeighborsOfStateChange(pos.offset(direction), this);
-			}
-		}
-	}
-
-	@Override
-	public int getWeakPower(BlockState state, IBlockReader blockAccess, BlockPos pos, Direction side) {
-		return state.get(LIT) && side.getAxis() != Axis.Y && state.get(FACING) == (checkAxis(state) ? side.getOpposite() : side.rotateYCCW()) ? 15 : 0;
-	}
-
-	@Override
-	public int getStrongPower(BlockState state, IBlockReader blockAccess, BlockPos pos, Direction side) {
-		return state.getWeakPower(blockAccess, pos, side);
-	}
-
-	@Override
-	public boolean canProvidePower(BlockState state) {
-		return true;
+		return super.getStateForPlacement(context).with(LIT, context.getWorld().isBlockPowered(context.getPos()));
 	}
 
 	@Override
 	public void neighborChanged(BlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos, boolean isMoving) {
 		if (!worldIn.isRemote) {
 			boolean flag = state.get(LIT);
-			if (flag == isBlockPowered(state, worldIn, pos)) {
-				if (flag) {
-					worldIn.setBlockState(pos, state.func_235896_a_(LIT), 2);
-				} else {
-					worldIn.getPendingBlockTicks().scheduleTick(pos, this, 4);
-				}
+			if (flag != worldIn.isBlockPowered(pos)) {
+				worldIn.setBlockState(pos, state.func_235896_a_(LIT), 2);
 			}
 		}
 	}
 
 	@Override
 	public void tick(BlockState state, ServerWorld worldIn, BlockPos pos, Random rand) {
-		if (!state.get(LIT) && !isBlockPowered(state, worldIn, pos)) {
-			worldIn.setBlockState(pos, state.func_235896_a_(LIT), 3);
+		if (state.get(LIT) && !worldIn.isBlockPowered(pos)) {
+			worldIn.setBlockState(pos, state.func_235896_a_(LIT), 2);
 		}
-	}
-
-	private static boolean isBlockPowered(BlockState state, World world, BlockPos pos) {
-		for (Direction direction : Direction.values()) {
-			if (world.isSidePowered(pos.offset(direction), direction) && !(direction.getAxis() != Axis.Y && state.get(FACING) == (checkAxis(state) ? direction : direction.getOpposite().rotateYCCW()))) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	private static boolean checkAxis(BlockState state) {
-		Direction facing = state.get(FACING);
-		CarvedSide side = state.get(CARVED_SIDE);
-		return facing.getAxis() == Axis.X && side == CarvedSide.X || facing.getAxis() == Axis.Z && side == CarvedSide.Z;
 	}
 
 	@Override
--- src/main/java/com/minecraftabnormals/autumnity/common/block/RedstoneJackOLanternBlock.java
+++ src/main/java/com/minecraftabnormals/autumnity/common/block/RedstoneJackOLanternBlock.java
@@ -7,9 +7,7 @@ import net.minecraft.block.RedstoneTorchBlock;
 import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.StateContainer;
-import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 
@@ -21,75 +19,30 @@ public class RedstoneJackOLanternBlock extends AutumnityJackOLanternBlock {
 
 	public RedstoneJackOLanternBlock(AbstractBlock.Properties properties) {
 		super(properties);
-		this.setDefaultState(this.getDefaultState().with(LIT, Boolean.TRUE));
+		this.setDefaultState(this.getDefaultState().with(LIT, Boolean.valueOf(false)));
 	}
 
 	@Nullable
 	@Override
 	public BlockState getStateForPlacement(BlockItemUseContext context) {
-		BlockState blockstate = this.getDefaultState().with(FACING, context.getPlacementHorizontalFacing().getOpposite());
-		return blockstate.with(LIT, !isBlockPowered(blockstate, context.getWorld(), context.getPos()));
-	}
-
-	@Override
-	public void onBlockAdded(BlockState state, World worldIn, BlockPos pos, BlockState oldState, boolean isMoving) {
-		for (Direction direction : Direction.values()) {
-			worldIn.notifyNeighborsOfStateChange(pos.offset(direction), this);
-		}
-	}
-
-	@Override
-	public void onReplaced(BlockState state, World worldIn, BlockPos pos, BlockState newState, boolean isMoving) {
-		if (!isMoving) {
-			for (Direction direction : Direction.values()) {
-				worldIn.notifyNeighborsOfStateChange(pos.offset(direction), this);
-			}
-		}
-	}
-
-	@Override
-	public int getWeakPower(BlockState state, IBlockReader blockAccess, BlockPos pos, Direction side) {
-		return state.get(LIT) && state.get(FACING) == side.getOpposite() ? 15 : 0;
-	}
-
-	@Override
-	public int getStrongPower(BlockState state, IBlockReader blockAccess, BlockPos pos, Direction side) {
-		return state.getWeakPower(blockAccess, pos, side);
-	}
-
-	@Override
-	public boolean canProvidePower(BlockState state) {
-		return true;
+		return this.getDefaultState().with(FACING, context.getPlacementHorizontalFacing().getOpposite()).with(LIT, Boolean.valueOf(context.getWorld().isBlockPowered(context.getPos())));
 	}
 
 	@Override
 	public void neighborChanged(BlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos, boolean isMoving) {
 		if (!worldIn.isRemote) {
 			boolean flag = state.get(LIT);
-			if (flag == isBlockPowered(state, worldIn, pos)) {
-				if (flag) {
-					worldIn.setBlockState(pos, state.func_235896_a_(LIT), 2);
-				} else {
-					worldIn.getPendingBlockTicks().scheduleTick(pos, this, 4);
-				}
+			if (flag != worldIn.isBlockPowered(pos)) {
+				worldIn.setBlockState(pos, state.func_235896_a_(LIT), 2);
 			}
 		}
 	}
 
 	@Override
 	public void tick(BlockState state, ServerWorld worldIn, BlockPos pos, Random rand) {
-		if (!state.get(LIT) && !isBlockPowered(state, worldIn, pos)) {
-			worldIn.setBlockState(pos, state.func_235896_a_(LIT), 3);
+		if (state.get(LIT) && !worldIn.isBlockPowered(pos)) {
+			worldIn.setBlockState(pos, state.func_235896_a_(LIT), 2);
 		}
-	}
-
-	private static boolean isBlockPowered(BlockState state, World world, BlockPos pos) {
-		for (Direction direction : Direction.values()) {
-			if (world.isSidePowered(pos.offset(direction), direction) && direction != state.get(FACING)) {
-				return true;
-			}
-		}
-		return false;
 	}
 
 	@Override

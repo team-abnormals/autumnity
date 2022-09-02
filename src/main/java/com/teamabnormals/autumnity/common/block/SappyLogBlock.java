467f1d58f06715a07bc611ce7a88dabb0d34692b
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2022-09-02 13:51:44 -0500

    Fix Bottling Sap in Creative

--- src/main/java/com/teamabnormals/autumnity/common/block/SappyLogBlock.java
+++ src/main/java/com/teamabnormals/autumnity/common/block/SappyLogBlock.java
@@ -1,11 +1,13 @@
 package com.teamabnormals.autumnity.common.block;
 
 import com.teamabnormals.autumnity.core.registry.AutumnityItems;
+import com.teamabnormals.blueprint.core.util.BlockUtil;
 import com.teamabnormals.blueprint.core.util.item.filling.TargetedItemCategoryFiller;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.NonNullList;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
@@ -17,6 +19,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraftforge.registries.RegistryObject;
 
@@ -32,33 +35,28 @@ public class SappyLogBlock extends RotatedPillarBlock {
 	}
 
 	@Override
-	public InteractionResult use(BlockState state, Level worldIn, BlockPos pos, Player player, InteractionHand handIn, BlockHitResult hit) {
-		ItemStack itemstack = player.getItemInHand(handIn);
-		if (itemstack.isEmpty()) {
-			return InteractionResult.PASS;
-		} else {
-			Item item = itemstack.getItem();
-			if (item == Items.GLASS_BOTTLE) {
-				if (!worldIn.isClientSide) {
-					if (!player.getAbilities().instabuild) {
-						ItemStack itemstack2 = new ItemStack(AutumnityItems.SAP_BOTTLE.get());
-						itemstack.shrink(1);
-						if (itemstack.isEmpty()) {
-							player.setItemInHand(handIn, itemstack2);
-						} else if (!player.getInventory().add(itemstack2)) {
-							player.drop(itemstack2, false);
-						}
-					}
-
-					worldIn.playSound(null, pos, SoundEvents.BOTTLE_FILL, SoundSource.BLOCKS, 1.0F, 1.0F);
-					worldIn.setBlock(pos, saplessBlock.get().defaultBlockState().setValue(RotatedPillarBlock.AXIS, state.getValue(RotatedPillarBlock.AXIS)), 11);
-				}
-
-				return InteractionResult.SUCCESS;
+	public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult result) {
+		ItemStack stack = player.getItemInHand(hand);
+		Item item = stack.getItem();
+		if (stack.is(Items.GLASS_BOTTLE)) {
+			stack.shrink(1);
+			level.playSound(player, player.getX(), player.getY(), player.getZ(), SoundEvents.BOTTLE_FILL, SoundSource.NEUTRAL, 1.0F, 1.0F);
+			if (stack.isEmpty()) {
+				player.setItemInHand(hand, new ItemStack(AutumnityItems.SAP_BOTTLE.get()));
+			} else if (!player.getInventory().add(new ItemStack(AutumnityItems.SAP_BOTTLE.get()))) {
+				player.drop(new ItemStack(AutumnityItems.SAP_BOTTLE.get()), false);
 			}
 
-			return InteractionResult.PASS;
+			level.gameEvent(player, GameEvent.FLUID_PICKUP, pos);
+			if (!level.isClientSide()) {
+				player.awardStat(Stats.ITEM_USED.get(item));
+			}
+
+			level.setBlockAndUpdate(pos, BlockUtil.transferAllBlockStates(state, this.saplessBlock.get().defaultBlockState()));
+			return InteractionResult.sidedSuccess(level.isClientSide);
 		}
+
+		return super.use(state, level, pos, player, hand, result);
 	}
 
 	@Override

6bba2d41f7a7aeee81a44bde26bcf57563c42c1e
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2022-10-20 14:04:09 -0500

    Fix Turkey Blocks not being mineable with knives

--- src/main/java/com/teamabnormals/autumnity/core/other/tags/AutumnityBlockTags.java
+++ src/main/java/com/teamabnormals/autumnity/core/other/tags/AutumnityBlockTags.java
@@ -3,12 +3,15 @@ package com.teamabnormals.autumnity.core.other.tags;
 import com.teamabnormals.autumnity.core.Autumnity;
 import com.teamabnormals.blueprint.core.util.TagUtil;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
 public class AutumnityBlockTags {
 	public static final TagKey<Block> SNAIL_SNACKS = blockTag("snail_snacks");
 	public static final TagKey<Block> SLIPPERY_SNAIL_GOO_BLOCKS = blockTag("slippery_snail_goo_blocks");
 	public static final TagKey<Block> MAPLE_LOGS = blockTag("maple_logs");
+
+	public static final TagKey<Block> MINEABLE_WITH_KNIFE = TagUtil.blockTag("farmersdelight", "mineable/knife");
 
 	private static TagKey<Block> blockTag(String name) {
 		return TagUtil.blockTag(Autumnity.MOD_ID, name);

410a2ce5ce7162e40d0a52fd37ef5e1c90d8b29f
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2023-03-16 16:01:31 -0500

    Show repair info in JEI

---
+++ src/main/java/com/teamabnormals/autumnity/integration/jei/AutumnityPlugin.java
@@ -0,0 +1,82 @@
+package com.teamabnormals.autumnity.integration.jei;
+
+import com.teamabnormals.autumnity.core.Autumnity;
+import com.teamabnormals.autumnity.core.other.AutumnityTiers;
+import com.teamabnormals.autumnity.core.registry.AutumnityItems;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.constants.RecipeTypes;
+import mezz.jei.api.recipe.vanilla.IJeiAnvilRecipe;
+import mezz.jei.api.recipe.vanilla.IVanillaRecipeFactory;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Ingredient;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+@JeiPlugin
+public class AutumnityPlugin implements IModPlugin {
+
+	@Override
+	public ResourceLocation getPluginUid() {
+		return new ResourceLocation(Autumnity.MOD_ID, Autumnity.MOD_ID);
+	}
+
+
+	@Override
+	public void registerRecipes(IRecipeRegistration registration) {
+		registration.addRecipes(RecipeTypes.ANVIL, getRepairRecipes(registration.getVanillaRecipeFactory()).toList());
+	}
+
+	private static Stream<RepairData> getRepairData() {
+		return Stream.of(new RepairData(AutumnityTiers.SNAIL_SHELL.getRepairIngredient(), new ItemStack(AutumnityItems.SNAIL_SHELL_CHESTPLATE.get())));
+	}
+
+	private static Stream<IJeiAnvilRecipe> getRepairRecipes(IVanillaRecipeFactory vanillaRecipeFactory) {
+		return getRepairData().flatMap(repairData -> getRepairRecipes(repairData, vanillaRecipeFactory));
+	}
+
+	private static Stream<IJeiAnvilRecipe> getRepairRecipes(RepairData repairData, IVanillaRecipeFactory vanillaRecipeFactory) {
+		Ingredient repairIngredient = repairData.getRepairIngredient();
+		List<ItemStack> repairables = repairData.getRepairables();
+
+		List<ItemStack> repairMaterials = List.of(repairIngredient.getItems());
+
+		return repairables.stream().mapMulti((itemStack, consumer) -> {
+			ItemStack damagedThreeQuarters = itemStack.copy();
+			damagedThreeQuarters.setDamageValue(damagedThreeQuarters.getMaxDamage() * 3 / 4);
+			ItemStack damagedHalf = itemStack.copy();
+			damagedHalf.setDamageValue(damagedHalf.getMaxDamage() / 2);
+
+			IJeiAnvilRecipe repairWithSame = vanillaRecipeFactory.createAnvilRecipe(List.of(damagedThreeQuarters), List.of(damagedThreeQuarters), List.of(damagedHalf));
+			consumer.accept(repairWithSame);
+
+			if (!repairMaterials.isEmpty()) {
+				ItemStack damagedFully = itemStack.copy();
+				damagedFully.setDamageValue(damagedFully.getMaxDamage());
+				IJeiAnvilRecipe repairWithMaterial = vanillaRecipeFactory.createAnvilRecipe(List.of(damagedFully), repairMaterials, List.of(damagedThreeQuarters));
+				consumer.accept(repairWithMaterial);
+			}
+		});
+	}
+
+	private static class RepairData {
+		private final Ingredient repairIngredient;
+		private final List<ItemStack> repairables;
+
+		public RepairData(Ingredient repairIngredient, ItemStack... repairables) {
+			this.repairIngredient = repairIngredient;
+			this.repairables = List.of(repairables);
+		}
+
+		public Ingredient getRepairIngredient() {
+			return repairIngredient;
+		}
+
+		public List<ItemStack> getRepairables() {
+			return repairables;
+		}
+	}
+}

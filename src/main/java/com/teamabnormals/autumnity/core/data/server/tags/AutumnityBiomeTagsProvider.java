15815d30eae752ac7ac1ce5f4be2453221bf00e5
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2023-03-16 13:39:03 -0500

    Biome tag tweaks

--- src/main/java/com/teamabnormals/autumnity/core/data/server/tags/AutumnityBiomeTagsProvider.java
+++ src/main/java/com/teamabnormals/autumnity/core/data/server/tags/AutumnityBiomeTagsProvider.java
@@ -3,6 +3,7 @@ package com.teamabnormals.autumnity.core.data.server.tags;
 import com.teamabnormals.autumnity.core.Autumnity;
 import com.teamabnormals.autumnity.core.other.tags.AutumnityBiomeTags;
 import com.teamabnormals.autumnity.core.registry.AutumnityBiomes;
+import com.teamabnormals.blueprint.core.other.tags.BlueprintBiomeTags;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.BiomeTagsProvider;
 import net.minecraft.tags.BiomeTags;
@@ -25,6 +26,7 @@ public class AutumnityBiomeTagsProvider extends BiomeTagsProvider {
 		this.tag(AutumnityBiomeTags.HAS_ORANGE_MAPLE_TREE).add(Biomes.DARK_FOREST);
 		this.tag(AutumnityBiomeTags.HAS_RED_MAPLE_TREE).add(Biomes.TAIGA, Biomes.WINDSWEPT_FOREST);
 
+		this.tag(BlueprintBiomeTags.IS_GRASSLAND).add(AutumnityBiomes.PUMPKIN_FIELDS.get());
 		this.tag(BiomeTags.IS_FOREST).add(AutumnityBiomes.MAPLE_FOREST.get());
 		this.tag(BiomeTags.IS_OVERWORLD).add(AutumnityBiomes.MAPLE_FOREST.get(), AutumnityBiomes.PUMPKIN_FIELDS.get());
 		this.tag(BiomeTags.HAS_RUINED_PORTAL_STANDARD).add(AutumnityBiomes.PUMPKIN_FIELDS.get());

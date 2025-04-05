1760cff039d250e7af7cb90d6f93edec9ef09bde
Markus1002 <51504784+Markus1002@users.noreply.github.com>
2025-04-06 01:10:02 +0300

    Colored maple leaf textures

--- src/main/java/com/teamabnormals/autumnity/core/data/client/AutumnityBlockStateProvider.java
+++ src/main/java/com/teamabnormals/autumnity/core/data/client/AutumnityBlockStateProvider.java
@@ -4,11 +4,7 @@ import com.teamabnormals.autumnity.core.Autumnity;
 import com.teamabnormals.autumnity.core.other.AutumnityBlockFamilies;
 import com.teamabnormals.blueprint.core.data.client.BlueprintBlockStateProvider;
 import net.minecraft.data.PackOutput;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.block.Block;
-import net.neoforged.neoforge.client.model.generators.ModelFile.UncheckedModelFile;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
-import net.neoforged.neoforge.registries.DeferredBlock;
 
 import static com.teamabnormals.autumnity.core.registry.AutumnityBlocks.*;
 
@@ -31,10 +27,10 @@ public class AutumnityBlockStateProvider extends BlueprintBlockStateProvider {
 		this.logBlocks(SAPPY_MAPLE_LOG, SAPPY_MAPLE_WOOD);
 		this.hangingSignBlocks(STRIPPED_MAPLE_LOG, MAPLE_HANGING_SIGNS.getFirst(), MAPLE_HANGING_SIGNS.getSecond());
 
-		this.mapleLeavesBlocks(MAPLE_LEAVES, MAPLE_LEAF_PILE);
-		this.mapleLeavesBlocks(YELLOW_MAPLE_LEAVES, YELLOW_MAPLE_LEAF_PILE);
-		this.mapleLeavesBlocks(ORANGE_MAPLE_LEAVES, ORANGE_MAPLE_LEAF_PILE);
-		this.mapleLeavesBlocks(RED_MAPLE_LEAVES, RED_MAPLE_LEAF_PILE);
+		this.leavesBlocks(MAPLE_LEAVES, MAPLE_LEAF_PILE);
+		this.leavesBlocks(YELLOW_MAPLE_LEAVES, YELLOW_MAPLE_LEAF_PILE);
+		this.leavesBlocks(ORANGE_MAPLE_LEAVES, ORANGE_MAPLE_LEAF_PILE);
+		this.leavesBlocks(RED_MAPLE_LEAVES, RED_MAPLE_LEAF_PILE);
 
 		this.crossBlockWithPot(MAPLE_SAPLING, POTTED_MAPLE_SAPLING);
 		this.crossBlockWithPot(YELLOW_MAPLE_SAPLING, POTTED_YELLOW_MAPLE_SAPLING);
@@ -44,11 +40,5 @@ public class AutumnityBlockStateProvider extends BlueprintBlockStateProvider {
 
 		this.woodworksBlocks(MAPLE_PLANKS, MAPLE_BOARDS, MAPLE_LADDER, MAPLE_BOOKSHELF, MAPLE_BEEHIVE, MAPLE_CHEST, TRAPPED_MAPLE_CHEST);
 		this.chiseledBookshelfBlock(CHISELED_MAPLE_BOOKSHELF, BOTTOM_BOOKSHELF_POSITIONS);
-	}
-
-	public void mapleLeavesBlocks(DeferredBlock<Block> leaves, DeferredBlock<Block> leafPile) {
-		this.simpleBlock(leaves.get(), this.models().getBuilder(name(leaves.get())).parent(new UncheckedModelFile(ResourceLocation.withDefaultNamespace("block/leaves"))).renderType("cutout_mipped").texture("all", blockTexture(MAPLE_LEAVES.get())));
-		this.blockItem(leaves);
-		this.leafPileBlock(leafPile, blockTexture(MAPLE_LEAVES.get()), true);
 	}
 }
--- src/main/java/com/teamabnormals/autumnity/core/other/AutumnityClientCompat.java
+++ src/main/java/com/teamabnormals/autumnity/core/other/AutumnityClientCompat.java
@@ -1,12 +1,6 @@
 package com.teamabnormals.autumnity.core.other;
 
-import com.teamabnormals.autumnity.client.model.SnailModel;
-import com.teamabnormals.autumnity.client.model.TurkeyModel;
-import com.teamabnormals.autumnity.client.renderer.entity.SnailRenderer;
-import com.teamabnormals.autumnity.client.renderer.entity.TurkeyEggRenderer;
-import com.teamabnormals.autumnity.client.renderer.entity.TurkeyRenderer;
 import com.teamabnormals.autumnity.core.registry.AutumnityBlocks;
-import com.teamabnormals.autumnity.core.registry.AutumnityEntityTypes;
 import com.teamabnormals.autumnity.core.registry.AutumnityItems;
 import com.teamabnormals.woodworks.core.Woodworks;
 import net.minecraft.client.Minecraft;
@@ -16,13 +10,9 @@ import net.minecraft.client.renderer.BiomeColors;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.level.FoliageColor;
 import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.client.event.EntityRenderersEvent.RegisterLayerDefinitions;
-import net.neoforged.neoforge.client.event.EntityRenderersEvent.RegisterRenderers;
 
 @EventBusSubscriber(modid = Woodworks.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class AutumnityClientCompat {
@@ -62,16 +52,12 @@ public class AutumnityClientCompat {
 		BlockColors blockcolors = Minecraft.getInstance().getBlockColors();
 		ItemColors itemcolors = Minecraft.getInstance().getItemColors();
 
-		blockcolors.register((state, world, pos, tintIndex) -> world != null && pos != null ? BiomeColors.getAverageFoliageColor(world, pos) : FoliageColor.getDefaultColor(), AutumnityBlocks.MAPLE_LEAVES.get(), AutumnityBlocks.MAPLE_LEAF_PILE.get());
-		blockcolors.register((state, world, pos, tintIndex) -> 12665871, AutumnityBlocks.RED_MAPLE_LEAVES.get(), AutumnityBlocks.RED_MAPLE_LEAF_PILE.get());
-		blockcolors.register((state, world, pos, tintIndex) -> 16745768, AutumnityBlocks.ORANGE_MAPLE_LEAVES.get(), AutumnityBlocks.ORANGE_MAPLE_LEAF_PILE.get());
-		blockcolors.register((state, world, pos, tintIndex) -> 16760576, AutumnityBlocks.YELLOW_MAPLE_LEAVES.get(), AutumnityBlocks.YELLOW_MAPLE_LEAF_PILE.get());
+		blockcolors.register((state, world, pos, tintIndex) -> world != null && pos != null ? BiomeColors.getAverageFoliageColor(world, pos) : 0xA1C440, AutumnityBlocks.MAPLE_LEAVES.get(), AutumnityBlocks.MAPLE_LEAF_PILE.get());
 
 		itemcolors.register((stack, tintIndex) -> {
 					BlockState blockstate = ((BlockItem) stack.getItem()).getBlock().defaultBlockState();
 					return blockcolors.getColor(blockstate, null, null, tintIndex);
 				},
-				AutumnityBlocks.MAPLE_LEAVES.get(), AutumnityBlocks.RED_MAPLE_LEAVES.get(), AutumnityBlocks.ORANGE_MAPLE_LEAVES.get(), AutumnityBlocks.YELLOW_MAPLE_LEAVES.get(),
-				AutumnityBlocks.MAPLE_LEAF_PILE.get(), AutumnityBlocks.RED_MAPLE_LEAF_PILE.get(), AutumnityBlocks.ORANGE_MAPLE_LEAF_PILE.get(), AutumnityBlocks.YELLOW_MAPLE_LEAF_PILE.get());
+				AutumnityBlocks.MAPLE_LEAVES.get(), AutumnityBlocks.MAPLE_LEAF_PILE.get());
 	}
 }
